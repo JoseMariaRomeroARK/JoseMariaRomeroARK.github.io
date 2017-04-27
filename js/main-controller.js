d24a5a219a3ca17edc442d78504700cb02c87fbe
Jose Romero <Jose Romero>
2017-04-27 19:54:59 +0100

    github compatible

--- js/main-controller.js
+++ js/main-controller.js
@@ -4,14 +4,17 @@ document.write("<script type='text/javascript' src='js/job-display-controller.js
 document.write("<script type='text/javascript' src='js/skill-display-controller.js'></script "+">");
 window.onload = function(){
 	var loc = window.location.pathname.split("/");
-	if(loc[loc.length -1] == "index.html"){
+	if (window.location.hostname = "https://josemariaromeroark.github.io/") {
+		if (loc[1]=="" || loc[1]=="index.html" ) {}
+	}else if(loc[loc.length -1] == "index.html" ){
 		var HTML_Tsk = document.getElementById("Tsk");
-		var HTML_Psk = document.getElementById("Psk");
 		HTML_Tsk.appendChild(create_tech_skill_set());
+		var HTML_Psk = document.getElementById("Psk");
 		HTML_Psk.appendChild(create_personal_skill_set());
 		diplay_jobs();
 		sizeController();
 	}
+	
 	
 }
 
