ae788ad3edd3584e5719c192c542dc61bc3aeab7
Jose Romero <Jose Romero>
2017-06-17 22:46:11 +0100

    jquery improvements

--- js/jquery-index.js
+++ js/jquery-index.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
+	toggleClass(isScrolledIntoView($('#animationTrigger')));
 	$(document).scroll(function(){//every time scroll is made...
 		var trigger = isScrolledIntoView($('#animationTrigger'));
+		toggleClass(trigger);
+	});
+	function toggleClass(trigger){
 		if (trigger) {
 			$('.level').addClass('active');
 		}else{
 			$('.level').removeClass('active');
 		}
-	});
+	}
 	function isScrolledIntoView(elem){
 	    var docViewTop = $(window).scrollTop(); //get the vertical scroll bar position
 	    var docViewBottom = docViewTop + $(window).height(); 
