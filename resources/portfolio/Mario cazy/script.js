81b6924bc58caf602715d74dbcb7bf5f739722d1
Jose Romero <Jose Romero>
2017-06-19 11:32:26 +0100

    Super Mario dpad

--- resources/portfolio/Mario cazy/script.js
+++ resources/portfolio/Mario cazy/script.js
@@ -1,36 +1,71 @@
 $(document).ready(function() {
    	placeCoin();
    	$('#mario').draggable();
+    $('#mario').mouseup(function(){
+        is_touching();
+    });
+    /*D-pad functionality*/
+    $('.arrow.l').click(function(){
+        mario_move("left");
+    });
+    $('.arrow.u').click(function(){
+        mario_move("up");
+    });
+    $('.arrow.r').click(function(){
+        mario_move("right");
+    });
+    $('.arrow.d').click(function(){
+        mario_move("down");
+    });
+    /*D-pad functionality end*/
+    
     $(document).keyup(function(key) {
     	//$('p#hint').html(key.key+"="+parseInt(key.which,10));
         switch(parseInt(key.which,10)) {
 			// A,Left arrow key pressed
 			case 65:
 			case 37:
-				$('#mario').animate({left: "-=15px"}, 'fast');
+				mario_move("left");
 				break;
 			// W,Up Arrow Pressed
 			case 87:	
 			case 38:
-				$('#mario').animate({top: "-=15px"}, 'fast');
+                mario_move("up");
 				break;
 			// D,Right Arrow Pressed
 			case 68:
 			case 39:
-				$('#mario').animate({left: "+=15px"}, 'fast');
+                mario_move("right");
 				break;
 			// S,Down Arrow Pressed
 			case 83:
 			case 40:
-				$('#mario').animate({top: "+=15px"}, 'fast');
+                mario_move("down");
 				break;
 		}
-		is_touching();
 	});
 
-    $('#mario').mouseup(function(){
-    	is_touching();
-    });
+    function mario_move(direction){
+        switch(direction) {
+            // A,Left arrow key pressed
+            case "left":
+                $('#mario').animate({left: "-=15px"}, 'fast');
+                break;
+            // W,Up Arrow Pressed
+            case "up":
+                $('#mario').animate({top: "-=15px"}, 'fast');
+                break;
+            // D,Right Arrow Pressed
+            case "right":
+                $('#mario').animate({left: "+=15px"}, 'fast');
+                break;
+            // S,Down Arrow Pressed
+            case "down":
+                $('#mario').animate({top: "+=15px"}, 'fast');
+                break;
+        }
+        is_touching();
+    }
 
     function placeCoin(){
     	var top = Math.floor(Math.random()*250);
